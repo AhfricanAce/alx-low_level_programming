155806068845c4754675800c444cef57175f6948
AhfricanAce <ahfricanace@gmail.com>
2023-07-17 19:12:22 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include stdio.h
+/**
+ * main - Print a new line program using puts function from standard io library
+ * Return: 0 (Done correctly)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
